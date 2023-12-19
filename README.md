ca17c3f523bc3ee3bb6d7a417e905e1b56b19dba
Linxiao Geng <linxiao.CN.geng@gmail.com>
2023-12-19 23:52:27 +0000

    README.md updated.

--- README.md
+++ README.md
@@ -2,3 +2,22 @@
 This is a repository to store all the code for a minimalist monitoring system for Energy Storing Devices, including a mockup database design, python code for a micro-service for querying hardware information, design documents and configuration files for the monitoring, C# code for data collection on the devices and various scripts for building the solution.
 
 In principle, people should be able to setup a working monitoring system out of the code and documentations from this repository.
+
+## Handling of Modbus Protocol
+There are quite a few dotnet projects implemented modbus protocol. However, most of them are already several years old. Due to the dizzling change of dotnet in the past years, finding one that's works with modern dotnet on Linux is not easy.
+
+In the end, I decided to go with NModbus4 as it appears to be the most used one.
+
+### Incorperate NModbus4
+
+Directly installing from Nuget resulted in some issues. However, The process of porting NModbus4 to modern dotnet is rather painless.
+The following steps has to be performed on each of the C# projects, first the NModbus4, then NModbus4.Serial, and then the rest projects.
+1. Old project file (.xproj file) has to be removed.
+2. Create empty project using dotnet CLI. (dotnet new console or dotnet new classlib, depending on the project)
+3. Add references to the newly created projects. NModbus4.Serial referenced NModbus4, then the other projects referenced these 2.
+4. Add references to external packages.
+    For SerialPort, you need "dotnet add package System.IO.Ports --version 8.0.0".
+    For Moq, you need "dotnet add package Moq --version 4.20.70".
+5. Then you can change the .sln file to refer to the new .csproj files.
+
+That's basically it. No code change is needed. 
--- datacollector/Program.cs
+++ datacollector/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Net;
+using System.Net.Sockets;
+using Modbus;
+using Modbus.Device;
 
 class Program
 {
+    private const int DefaultPort = 502;
     static void Main(string[] args)
     {
+	if (args.Length > 0)
+        {
+            switch (args[0].ToLower())
+            {
+                case "--version":
+                    PrintVersion();
+                    return;
+                case "--help":
+                    PrintHelp();
+                    return;
+                // Add other cases for different options
+                default:
+                    Console.WriteLine("Unknown option. Use '--help' for usage information.");
+                    return;
+            }
+	}
         const string configFilePath = "collector.conf";
 
         try
@@ -18,19 +39,94 @@ class Program
             }
 
             // Proceed with the rest of the program
+
+	    string slaveIpsStr=config["General"]["slaveIps"];
+	    string[] slaveIps=slaveIpsStr.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+		    .Select(item => item.Trim())
+		    .ToArray();
+
+	    ModbusTcpMasterReadInputs(slaveIps[0]);
         }
         catch (Exception ex)
         {
             Console.WriteLine($"Error reading configuration: {ex.Message}");
             Environment.Exit(1); // Exit with a non-zero status to indicate an error
         }
+
+	
+
     }
 
     static bool IsValidConfiguration(Dictionary<string, Dictionary<string, string>> config)
     {
         // Implement validation logic here
-        return config.ContainsKey("Input") && config.ContainsKey("Process") && config.ContainsKey("Process");
+        return config.ContainsKey("Input") && config.ContainsKey("Process") && config.ContainsKey("Process")&& config.ContainsKey("General");
         // Potentially more checks...
+    }
+    /// <summary>
+    ///     Simple Modbus TCP master read inputs example.
+    /// </summary>
+    public static void ModbusTcpMasterReadInputs(string connectionString)
+    {
+
+	if (string.IsNullOrWhiteSpace(connectionString))
+        {
+            throw new ArgumentException("Connection string cannot be null or empty.", nameof(connectionString));
+        }
+
+        string ipAddressStr;
+        int port;
+
+        int colonIndex = connectionString.IndexOf(':');
+        if (colonIndex >= 0)
+        {
+            ipAddressStr = connectionString.Substring(0, colonIndex);
+            if (!int.TryParse(connectionString.Substring(colonIndex + 1), out port))
+            {
+                throw new FormatException("Invalid port number.");
+            }
+        }
+        else
+        {
+            ipAddressStr = connectionString;
+            port = DefaultPort;
+        }
+
+        if (!IPAddress.TryParse(ipAddressStr, out IPAddress ipAddress))
+        {
+            throw new FormatException("Invalid IP address.");
+        }
+
+
+        using (TcpClient client = new TcpClient(ipAddressStr, port))
+        {
+            ModbusIpMaster master = ModbusIpMaster.CreateIp(client);
+
+            // read five input values
+            ushort startAddress = 100;
+            ushort numInputs = 5;
+            bool[] inputs = master.ReadInputs(startAddress, numInputs);
+
+            for (int i = 0; i < numInputs; i++)
+            {
+                Console.WriteLine($"Input {(startAddress + i)}={(inputs[i] ? 1 : 0)}");
+            }
+        }
+    }
+
+    static void PrintVersion()
+    {
+        Console.WriteLine("DataCollector version 1.0.0");
+        // Replace with actual version retrieval logic if necessary
+    }
+
+    static void PrintHelp()
+    {
+        Console.WriteLine("DataCollector Usage:");
+        Console.WriteLine("  --version   Show the version information.");
+        Console.WriteLine("  --help      Show this help message.");
+        Console.WriteLine("  --modpoll   Use modpoll as external ModBus handler");
+        // Add other options and their descriptions
     }
 }
 
